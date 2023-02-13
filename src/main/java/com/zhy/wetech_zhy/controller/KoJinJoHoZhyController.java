55250026e4bda7d9469ff246d3eb716f0482ebab
RigHakiko <add-122@hotmail.com>
2023-02-13 17:16:08 +0900

    注释掉controller的try与catch部分

--- src/main/java/com/zhy/wetech_zhy/controller/KoJinJoHoZhyController.java
+++ src/main/java/com/zhy/wetech_zhy/controller/KoJinJoHoZhyController.java
@@ -25,21 +25,23 @@ public class KoJinJoHoZhyController {
     @CrossOrigin
     public JsonResult save(@RequestBody KoJinJoHoZhy koJinJoHoZhy){
         JsonResult<Void> result = new JsonResult<>();
-        try {
-            koJinJoHoZhyService.save(koJinJoHoZhy);
-//            如果成功，将状态设置为200
-            //成功した場合、ステータスを200に設定する
-            result.setState(200);
-            System.out.println("success");
-        } catch (Exception e){
-//            如果失败, 将状态设置为4000
-            //失敗した場合は、ステータスを4000に設定。
-            result.setState(4000);
-//              失败情况写入错误信息
-            //エラーメッセージの書き込み
-            result.setMessage("异常发生");
-            System.out.println("fail");
-        }
+
+//        try {
+//            koJinJoHoZhyService.save(koJinJoHoZhy);
+////            如果成功，将状态设置为200
+//            //成功した場合、ステータスを200に設定する
+//            result.setState(200);
+//            System.out.println("success");
+//        } catch (Exception e){
+////            如果失败, 将状态设置为4000
+//            //失敗した場合は、ステータスを4000に設定。
+//            result.setState(4000);
+////              失败情况写入错误信息
+//            //エラーメッセージの書き込み
+//            result.setMessage("异常发生");
+//            System.out.println("fail");
+//        }
+
         return result;
     }
 }
